c518f454494ad35caf480a8f3ae604003e9caefc
tlegen <>
2024-02-26 23:54:55 +0500

    добавляет элементы шаблона, устанавливает скипты

--- app/assets/site/app.js
+++ app/assets/site/app.js
@@ -2,3 +2,16 @@
 import './scss/app.scss';
 
 /* js */
+const $ = require('jquery');
+// это "модифицирует" модуль jquery: добавляя к нему поведение
+// модуль bootstrap ничего не экспортирует/возвращает
+global.bootstrap = require('bootstrap');
+
+import bsCustomFileInput from 'bs-custom-file-input';
+import {logPlugin} from "@babel/preset-env/lib/debug";
+
+bsCustomFileInput.init();
+
+import 'owl.carousel';
+
+require('./js/eshop');
